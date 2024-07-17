1f5bcaa3b3ce6ea5e4ef721a9270af53e8dca730
fckolob <131497920+fckolob@users.noreply.github.com>
2024-07-16 22:47:48 -0300

    javascript strating

--- scripts/aluminio.js
+++ scripts/aluminio.js
@@ -1,17 +1,24 @@
-botones
+//botones
 
 const botonCorrediza = document.querySelector("#botonCorrediza");
 const botonBatiente = document.querySelector("#botonBatiente");
+const botonAtrasCorrediza = document.querySelector("#atras-corrediza");
+const botonAtrasBatiente = document.querySelector("#atras-batiente");
 const botonesCorrediza = document.querySelectorAll(".corrediza");
 const botonesBatiente = document.querySelectorAll(".batiente");
 
+
+
 botonesCorrediza.forEach(boton => {
     boton.classList.add("esconderBoton");
-  });
+});
 
 botonesBatiente.forEach(boton => {
     boton.classList.add("esconderBoton");
-  });
+});
+
+
+
 
 
 
@@ -28,7 +35,11 @@ botonCorrediza.addEventListener("click", () => {
     botonesCorrediza.forEach(boton => {
         boton.classList.remove("esconderBoton");
         boton.classList.add("boton");
+        
     });
+
+    botonAtrasCorrediza.classList.remove("esconderBoton");
+    botonAtrasCorrediza.classList.add("boton");
 });
 
 botonBatiente.addEventListener("click", () => {
@@ -39,7 +50,37 @@ botonBatiente.addEventListener("click", () => {
         boton.classList.remove("esconderBoton");
         boton.classList.add("boton");
     });
+    botonAtrasBatiente.classList.remove("esconderBoton");
+    botonAtrasBatiente.classList.add("boton");
 });
 
+botonAtrasCorrediza.addEventListener("click", () => {
+    botonesCorrediza.forEach(boton => {
+        boton.classList.remove("boton");
+        boton.classList.add("esconderBoton");
+    });
+    botonAtrasCorrediza.classList.remove("boton");
+    botonAtrasCorrediza.classList.add("esconderBoton");
 
+    botonBatiente.classList.remove("esconderBoton");
+    botonBatiente.classList.add("boton");
+    botonCorrediza.classList.remove("esconderBoton");
+    botonCorrediza.classList.add("boton");
+
+    
+})
+
+botonAtrasBatiente.addEventListener("click", () => {
+    botonesBatiente.forEach(boton => {
+        boton.classList.remove("boton");
+        boton.classList.add("esconderBoton");
+    });
+
+    botonAtrasBatiente.classList.remove("boton");
+    botonAtrasBatiente.classList.add("esconderBoton");
+    botonBatiente.classList.remove("esconderBoton");
+    botonBatiente.classList.add("boton");
+    botonCorrediza.classList.remove("esconderBoton");
+    botonCorrediza.classList.add("boton");
+})
 
